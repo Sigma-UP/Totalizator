78529d6ac063040ca56ccba6031d921918cad2b5
Sigma-UP <mynametrump@gmail.com>
2022-08-19 19:22:08 +0300

    create empty NewTask page

---
+++ src/components/NewTask/index.jsx
@@ -0,0 +1,10 @@
+import { Grid } from '@mui/material';
+
+const NewTask = () => {
+    return(
+        <Grid container direction="column" sx={{width:'100%', alignItems: 'center'}}>
+        </Grid>
+    )
+}
+export default NewTask;
+ 
