b8380a05b2e06857c148c918784250f78634e2f3
Sigma-UP <mynametrump@gmail.com>
2022-08-31 20:50:06 +0300

    added props in TaskItem

--- src/components/AllTasks/TaskItem/index.jsx
+++ src/components/AllTasks/TaskItem/index.jsx
@@ -1,19 +1,25 @@
 import { Grid } from '@mui/material';
 import styles from './style.module.css';
 
-const TaskItem = () => {
+//TODO:
+//- when description is over than its field, description displayed
+//cutted to not overflow component
+//- next redaction of app should provide an action bar. Probably it will
+//be realized by adding buttons inside taskitem
+
+const TaskItem = (props) => {
     return(
         <Grid container direction="column" className={styles.item}>
             <Grid container item>
                 <Grid item xs={9} sx={{fontWeight: '500'}}>
-                    TASK name
+                    {props.headline}
                 </Grid>
                 <Grid item xs sx={{textAlign:'right', fontWeight: '500'}}>
-                    12:40PM
+                    {props.deadline}
                 </Grid>
             </Grid>
             <Grid item sx={{fontWeight: '400', color:'darkgray'}}>
-                Lorem
+                {props.description}
             </Grid>
         </Grid>
     )
