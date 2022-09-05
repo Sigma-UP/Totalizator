e617de0fb2e2f72102907b7451583cfa813fbf34
Sigma-UP <mynametrump@gmail.com>
2022-09-05 23:35:52 +0300

    changed data management

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Grid } from '@mui/material';
 import { Container } from '@mui/system';
 import Header from './components/Header';
 import NewTask from './components/NewTask';
 import AllTasks from './components/AllTasks';
 import './App.css';
-import { tasksData } from './components/mock/data';
 
 const App = (props) => {
   return (
-    <BrowserRouter>
       <Grid className="app-wrapper">
         <Header/>
         <Container sx={{padding:'16px', paddingTop: '0px', display:'flex', justifyContent:'center', alignItems:'center'}}>
           <Routes>
             <Route path="/new" element={<NewTask />} />
-            <Route path="/all" element={<AllTasks data={tasksData}/>} />
+            <Route path="/all" element={<AllTasks data={props.state.tasksData}/>} />
           </Routes>  
         </Container>
       </Grid>
-    </BrowserRouter>
   );
 }
 
---
+++ src/redux/state.js
@@ -0,0 +1,9 @@
+let state = {
+    tasksData: [
+        {headline:'React', description: 'Finish learn React.', deadline:'12:05PM'},
+        {headline:'Olya', description: 'I love you!', deadline:'Endless'},
+        {headline:'Task Manager', description: 'Finish task.', deadline:'12:05PM'}
+    ]
+}
+
+export default state;
